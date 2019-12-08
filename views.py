3098c01e4319a0348045c2779adbcbad7607e9d1
dekeyel <dekeyel@dekeyel.ru>
2019-12-08 20:04:18 +0300

    pochti gotovo

--- views.py
+++ views.py
@@ -1,27 +1,37 @@
 import os
 import re
-
+from chardet.universaldetector import UniversalDetector
 
 class File:
     def __init__(self, file_name):
         self.file_name = file_name
 
+        detector = UniversalDetector()
+        with open(self.file_name, 'rb') as lines:
+            for line in lines:
+                detector.feed(line)
+                if detector.done:
+                    break
+        detector.close()
+
+        self.encoding = detector.result['encoding']
+
     def __new__(cls, file_name):
         try:
-            open(file_name, 'r')
+            open(file_name, 'rb')
             return object.__new__(cls)
         except:
             print('File {} is not exists'.format(file_name))
             return None
 
     def write(self, text):
-        with open(self.file_name, 'w') as F:
-            print('File: {}'.format(self.file_name))
+        print('File: {}'.format(self.file_name))
+        with open(self.file_name, 'wb') as F:
             F.write(text)
 
     def __str__(self):
-        with open(self.file_name, 'r') as lines:
-            string = 'File: {}'.format(self.file_name)
+        string = ''
+        with open(self.file_name, 'rb') as lines:
             for line in lines:
                 string = '{}{}'.format(string, line)
 
@@ -58,7 +68,32 @@ class Dir:
             files = '{}, {}'.format(files, file)
         return '{}\n{}'.format(self.path, files)
 
+class Convert:
+    def __init__(self):
+        self.encoding = [
+            'utf-8',
+            'cp500',
+            'utf-16',
+            'GBK',
+            'windows-1251',
+            'ASCII',
+            'US-ASCII',
+            'Big5'
+        ]
+
+    def show_encode(self, text):
+        detector = UniversalDetector()
+        detector.feed(text)
+        detector.close()
+
+        print(detector.result)
+
+    def convert(self, text, encode):
+        return text.encode(encode)
 
 if __name__ == '__main__':
     a = Dir('/home/dekeyel/Projects/labs5sem/dima/oop/kurs')
-    print(a.show(0))
+    b = Convert()
+    c = File('test.txt')
+    c.write(b.convert(c.__str__(), b.encoding[2]))
+    #print(type(b'dawdawdaw'))
